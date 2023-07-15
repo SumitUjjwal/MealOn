b49065703886caf1f23d37350e3c079c7768ea6f
SumitUjjwal <sumitonboard@gmail.com>
2023-07-16 00:49:25 +0530

    user routes

---
+++ routes/userRouter.py
@@ -0,0 +1,13 @@
+from flask import Blueprint
+from controllers.user import place_order
+from middleware.authenticator import token_required
+
+# Create a new Blueprint instance
+user = Blueprint('user', __name__)
+
+# Defined Routes for the user
+
+@user.route('/orders/place', methods=['POST'])
+@token_required
+def place_order_route():
+    return place_order()
